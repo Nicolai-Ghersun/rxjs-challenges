1bb317966eadb240f36fd3e5a8a4912fcfdb60fa
Ghersun Nicolai <ghershun@gmail.com>
2023-12-18 11:16:00 +0200

    challenge 4 tmp commit

--- src/app/components/challenge-4/challenge-4.component.ts
+++ src/app/components/challenge-4/challenge-4.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, Inject, inject } from "@angular/core";
 import { LoadingService } from "../../loading.service";
-import { Subject, animationFrameScheduler, concatMap, filter, from, interval, map, merge, mergeMap, of, pairwise, range, repeat, scan, share, startWith, switchMap, take, tap } from "rxjs";
+import { Subject, animationFrameScheduler, concat, concatMap, filter, finalize, from, interval, map, merge, mergeMap, of, pairwise, race, range, repeat, scan, share, skipWhile, startWith, switchMap, take, tap } from "rxjs";
 import { CommonModule } from "@angular/common";
 
 @Component({
@@ -11,32 +11,29 @@ import { CommonModule } from "@angular/common";
   ],
   template: `
   <blockquote>
-  <p>
-    Here you have <code>loadingService</code> with <code>load</code> method.
-  </p>
+    <p>
+      Here you have <code>loadingService</code> with <code>load</code> method.
+    </p>
 
-  <p>
-    Method emits <code>number</code> of % loaded or <code>string</code> with final result.
-  </p>
+    <p>
+      Method emits <code>number</code> of % loaded or <code>string</code> with final result.
+    </p>
 
-  <p>
-    Show progress bar with loaded % value until result
-  </p>
-</blockquote>
+    <p>
+      Show progress bar with loaded % value until result
+    </p>
+  </blockquote>
 
-<button class="button" (click)="onButtonClick()">
-  Start loading
-</button>
+  <button class="button" (click)="onButtonClick()">
+    Start loading
+  </button>
 
-<!-- <p *ngIf="true; else loading">
-  {{'final result'}}
-</p> -->
-
-@if (progress$ | async; as progress) {
-  <progress class="progress" [style.width.%]="progress"></progress>
-  <div class="loading">Loading...</div>
-}
-
+  @if (progress$ | async; as progress) {
+    <progress class="progress" [style.width.%]="progress"></progress>
+    <div class="loading">Loading...</div>
+  } @else {
+    {{'final result'}}
+  }
   `,
   styleUrl: './challenge-4.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -47,18 +44,21 @@ export class Challenge4Component {
   readonly submit$: Subject<void> = new Subject();
   readonly request$ = this.submit$.pipe(switchMap(() => this.loadingService.load()), startWith(0),share());
 
-  readonly finished$ = this.request$.pipe(filter(v=>v != 'number'),map(v=>0));
-  readonly progress$ = merge(
-    //this.finished$, 
-    this.request$.pipe(
-      startWith(0),
-      filter((v): v is number => typeof v === 'number'), 
-      pairwise<number>(),
-      mergeMap(([prev, current]: [number, number]) => {
-        return from(Array.from({length: (current - prev) / 2 + 1 },(v, i) => prev + i * 2))
-          .pipe(concatMap(value => interval(100, animationFrameScheduler).pipe(take(1), map(() => value))))
-      })
-    ));
+  readonly finished$ = this.request$.pipe(skipWhile(v => typeof v === 'number'), tap(console.log));
+  readonly progress$ =
+    race(
+      this.finished$,
+      this.request$.pipe(
+        startWith(0),
+        filter((v): v is number => typeof v === 'number'),
+        pairwise<number>(),
+        switchMap(([prev, current]: [number, number]) => {
+          return from(Array.from({length: (current - prev) / 1 + 1 },(v, i) => prev + i * 1))
+            .pipe(concatMap(value => interval(50, animationFrameScheduler).pipe(take(1), map(() => value))))
+        }),
+      ),
+    
+  ).pipe(tap(console.log));
       
   onButtonClick() {
     this.submit$.next();
